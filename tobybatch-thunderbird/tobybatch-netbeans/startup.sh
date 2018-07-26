0ac85621fdb4fffc0e896a268b90dfc6c7d782c7
Toby Batch <tobias@neontribe.co.uk>
2018-07-26 06:09:48 +0100

    removed bad overflowed files

--- tobybatch-thunderbird/tobybatch-netbeans/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sed -i "s/1000:1000/${UID}:${GID}/g" /etc/passwd
-sed -i "s/1000/${GID}/g" /etc/group
-chown developer:developer -R /home/developer
-su - developer -c /usr/local/netbeans-8.2/bin/netbeans
