7e32cacef0cda49006bd0bb7e66027b7635da748
Toby Batch <tobias@neontribe.co.uk>
2018-09-17 18:33:45 +0100

    Fixed db start up check

--- compose-kimai/kimai/dbtest.php
+++ compose-kimai/kimai/dbtest.php
@@ -3,15 +3,25 @@
 
 $DATABASE_URL = $argv[1];
 
-$url = parse_url($DATABASE_URL);
+if (strpos($DATABASE_URL, 'mysql') === 0) {
+    $url = parse_url($DATABASE_URL);
+    $dbscheme = $url['scheme'];
+    $dbhost = $url['host'];
+    $dbuser = $url['user'];
+    $dbpass = $url['pass'];
+    $dbname = substr($url['path'], 1);
 
-$dbscheme = $url['scheme'];
-$dbhost = $url['host'];
-$dbuser = $url['user'];
-$dbpass = $url['pass'];
-$dbname = substr($url['path'], 1);
-$connection = new PDO("mysql:dbname=$dbname;host=$dbhost", $dbuser, $dbpass);
+    $connection = new PDO("mysql:dbname=$dbname;host=$dbhost", $dbuser, $dbpass);
+    $stmt = $connection->query("SHOW TABLES");
+    echo $stmt->rowCount();
+    exit;
+}
+elseif (strpos($DATABASE_URL, 'sqlite') === 0) {
+    $connection = new PDO("$DATABASE_URL");
+    $stmt = $connection->query("SELECT  `name` FROM sqlite_master WHERE `type`='table' ORDER BY name");
+    echo $stmt->rowCount();
+    exit;
+}
 
-$stmt = $connection->query("SHOW TABLES");
-echo $stmt->rowCount();
+die('Unknown SQL scheme');
 
--- compose-kimai/kimai/startup.sh
+++ compose-kimai/kimai/startup.sh
@@ -3,6 +3,7 @@
 cd /opt/kimai
 
 count=$(php /usr/local/bin/dbtest.php $DATABASE_URL)
+echo "Database has '$count' tables."
 if [ "$count" -lt 10 ]; then
     echo "DB not ready, installing..."
     composer install --no-dev --optimize-autoloader
@@ -15,6 +16,7 @@ if [ "$count" -lt 10 ]; then
     chown -R www-data:www-data var
 
     if [ ! -z "$ADMIN_USER" ] && [ ! -z "$ADMIN_PASS" ] && [ ! -z "$ADMIN_MAIL" ]; then
+        echo "Creating admin user $ADMIN_USER $ADMIN_MAIL $ADMIN_PASS"
         bin/console kimai:create-user $ADMIN_USER $ADMIN_MAIL ROLE_SUPER_ADMIN $ADMIN_PASS
     fi
 fi
